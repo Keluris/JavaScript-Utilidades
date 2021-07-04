e775712382b88d45570a1c1d58686816394d696e
Lucas Lourenco <60081738+Keluris@users.noreply.github.com>
2021-07-04 20:52:23 -0300

    Template String Exemple
    Exemplo de Template String

---
+++ Template String.js
@@ -0,0 +1,21 @@
+/*
+Lucas Lourenco tem 18 anos, pesa 87 kg
+tem 1.87 de altura e seu IMC é de Nvalor
+Lucas Lourenco nasceu em 2002
+*/
+const nome = 'Lucas';
+const sobrenome = 'Lourenco';
+const idade = 18;
+const peso = 87;
+const alturaEmM = 1.87; // Corrigido para metros (ao invés de cm)
+let indiceMassaCorporal; // peso / (altura * altura)
+let anoNascimento;
+ 
+indiceMassaCorporal = peso / (alturaEmM * alturaEmM);
+anoNascimento = 2021 - idade;
+ 
+// template strings
+ 
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`);
+console.log(`tem ${alturaEmM} de altura e seu IMC é de ${indiceMassaCorporal}`);
+console.log(`${nome} nasceu em ${anoNascimento}.`);
